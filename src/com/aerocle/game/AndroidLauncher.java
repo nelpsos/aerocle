d89d72d20e01ce904dc1f88a88e004d17f816a0c
nelpsos <nelpsos@gmail.com>
2024-03-18 22:17:17 +0900

    feature: 환경 구성

---
+++ src/com/aerocle/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.aerocle.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.aerocle.game.Aerocle;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Aerocle(), config);
+	}
+}
